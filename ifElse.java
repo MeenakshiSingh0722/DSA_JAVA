12844c1997cae8da187e464d5282d05c6d45b3ec
Meenakshi Singh <meenakshisingh0722@gmail.com>
2026-04-28 23:59:56 +0530

    Add files via upload

---
+++ ifElse.java
@@ -0,0 +1,136 @@
+import java.util.Scanner;
+import java.util.Scanner.*;
+public class ifElse {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+//        System.out.println("Enter the number ");
+//        int n=sc.nextInt();
+        //(1)print absolute value
+//        if (n<=0) n=-n;
+//        System.out.println(n);
+//        if(n>=0) System.out.println(n);
+//        else System.out.println(-n);
+
+        // (2)take real number and check if it is int or not
+//        double n=sc.nextDouble();
+//        int x=(int)n;
+//        if(n-x<=0) System.out.println("Int value");
+//        else System.out.println(" not Int value");
+
+        //(3) seller profit loss and how much profit he made
+//        System.out.println("Enter selling price:");
+//        int s=sc.nextInt();
+//        System.out.println("Enter cost price:");
+//        int c=sc.nextInt();
+//        if(s>c){
+//            System.out.println("profit is "+(s-c));
+//        }else if(s<c){
+//            System.out.println("loss is "+(c-s));
+//        }else{
+//            System.out.println("No profit No loss");
+//        }
+
+        //(4) take positive int and tell if 4 digit number or not
+//        System.out.println("Enter 4 digit number:");
+//        int n=sc.nextInt();
+//        if(n>999 && n<10000){
+//            System.out.println(" 4 digit number");
+//        }else{
+//            System.out.println("Number is not 4 digit number ");
+//        }
+
+        //(5) TAKE 3 input and tell if they can be sides of tringle or not
+//        System.out.print("Enter a: ");
+//        int a= sc.nextInt();
+//        System.out.print("Enter b: ");
+//        int b= sc.nextInt();
+//        System.out.print("Enter c: ");
+//        int c= sc.nextInt();
+//        if(a+b>c && a+c>b && b+c>a){
+//            System.out.println("valid tringle");
+//        }else{
+//            System.out.println("invalid tringle");
+//        }
+
+        //(6) divisible by 5 ,3
+//        System.out.print("Enter a number: ");
+//        int n=sc.nextInt();
+//        ifS(n%3==0 && n%5==0){
+//            System.out.println("Divisible by 5 and 3 both");
+//        }else if(n%3==0){
+//            System.out.println("Divisible by 3 only");
+//        }else if(n%5==0){
+//            System.out.println("Divisible by 5 only");
+//        }else{
+//            System.out.println("not Divisible by 3 or 5");
+//        }
+
+        //(7)find perimeter and area which is greater of rectangle
+//        System.out.println("Enter l: ");
+//        int l= sc.nextInt();
+//        System.out.println("Enter b:");
+//        int b= sc.nextInt();
+//        if((l*b)>(2*(l+b))){
+//            System.out.println("Area is greater");
+//        }else{
+//            System.out.println("perimeter is greater");
+//        }
+
+        //(8) find quadrant
+//        System.out.println("Enter x :");
+//        int x=sc.nextInt();
+//        System.out.println("Enter y :");
+//        int y=sc.nextInt();
+//        if (x == 0 && y == 0) {
+//            System.out.println("point is on origin");
+//        }else if(x>0 && y==0){
+//            System.out.println("point is on x axis");
+//        }else if(x==0 && y>0){
+//            System.out.println("point is on y axis");
+//        }else if(x>0 && y>0){
+//            System.out.println("point is on 1st quadrant");
+//        }else if(x<0 && y>0){
+//            System.out.println("point is on 2nd quadrant");
+//        } else if(x<0 && y<0) {
+//            System.out.println("point is on 3rd quadrant");
+//        }else if(x>0 && y<0){
+//            System.out.println("point is on 4th quadrant");
+//        }
+
+        //(9) take 3 input and print greatest of them
+//        int a=sc.nextInt();
+//        int b=sc.nextInt();
+//        int c=sc.nextInt();
+//        if(a>b && a>c) System.out.println("A is greatest number "+ a );
+//        else if(b>a && b>c) System.out.println("B is greatest number "+b);
+//        else if(c>a && c>b) System.out.println("C is greatest number "+c);
+//        else System.out.println("All numbers are equal");
+        // Second logic
+//        if(a>=b) {
+//            if (a >= c) System.out.println(a);
+//            else System.out.println(c);
+//        }else if(a<b) {
+//            if (b >= c) System.out.println(b);
+//            else System.out.println(c);
+//        }
+
+        //ternary operator
+//        System.out.println("Enter a number:");
+//        int n =sc.nextInt();
+//        System.out.println(n+" "+((n%2==0)? "Even": "odd"));
+
+        //use : ek int bnakr 1(+ve) and 0(-ve) dalna
+//        System.out.print("Enter number: ");
+//        int a=sc.nextInt();
+//        int grp =(a>=0)? 1:0;
+//        System.out.print(grp);
+
+//        int a=sc.nextInt();
+//        int b=sc.nextInt();
+//        int c=sc.nextInt();
+//        System.out.print((a>b)? ((a>c)? a:c):(b>c)? b:c);
+
+    }
+}
+
+
---
+++ methods.java
@@ -0,0 +1,89 @@
+import javax.imageio.plugins.jpeg.JPEGHuffmanTable;
+import java.util.Scanner;
+
+public class methods {
+    //syntax
+//    public static void meesh(){
+//        System.out.println("im nepali momo");
+//    }
+//    public static void main(String [] args){
+//        meesh();
+//    }
+
+    //parameters and arguments
+//    public static void sum(int a,int b){
+//        System.out.println(a+b);
+//    }
+//    public static void main(String args[]){
+//    sum(5,6 );
+//    }
+
+    //max of 3 num
+//    public static void max(int a,int b, int c){
+//        if(a>=b && a>=c) System.out.println(a);
+//        else if(b>=a && b>=c) System.out.println(b);
+//        else System.out.println(c);
+//
+//    }    public static void main(String [] args){
+//        max(10,10,20);
+//    }
+
+    //return type
+//    public static int phugga(int a){
+//        System.out.println("nepali momo");
+//        if(a>0) return 9;
+//        else return 10;
+//    }
+//    public static void main (String[] args){
+//        phugga(-7);
+//        System.out.println(phugga(5));
+//        int x=phugga(8);
+//        System.out.println(x+3);
+//    }
+
+    //take n and r and print ncr and npr
+//    public static int fact(int x){
+//        int f=1;
+//        for(int i=1;i<=x;i++){
+//            f=f*i;
+//        }return f;
+//    }
+//    public static void main(String [] args){
+//        Scanner sc=new Scanner (System.in);
+//        int n=sc.nextInt();
+//        int r=sc.nextInt();
+//
+//        int ncr=fact(n)/fact(r)*fact(n-r);
+//        System.out.println(ncr);
+//        int npr=fact(n)/fact(r);
+//        System.out.println(npr); }
+
+    //pass by value and pass by reference
+//    public static void change(int x){
+//        x=10;
+//    }
+//    public static void main(String[] args){
+//        int x=6;
+//        System.out.println(x);
+//        change(x);
+//        System.out.println(x);
+//    }
+
+    //swap 2 number
+//    public static void main(String [] args){
+//        Scanner sc=new Scanner (System.in);
+//        int a=sc.nextInt();
+//        int b=sc.nextInt();
+//        //using third variable
+////        int temp=a;
+////        a=b;
+////        b=temp;
+////        System.out.println(a+" "+b);
+//
+//        //without using third variable
+//        a=a+b;
+//        b=a-b;
+//        a=a-b;
+//        System.out.println(a+" "+b);
+//    }
+}
