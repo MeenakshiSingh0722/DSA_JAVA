33eb52e1007524e777834aec89b0abb811cfa115
Meenakshi Singh <meenakshisingh0722@gmail.com>
2026-04-29 00:01:10 +0530

    Add files via upload

---
+++ passingArraysToMethods.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class passingArraysToMethods {
+    public static void main(String args[]){
+//        int x[]={1,4,6,7,3};
+//        System.out.println(x[3]);
+//        change(x);
+//        System.out.println(x[3]);
+//    }
+//    public static void change(int[] x){
+//        x[3]=20;
+
+//        int[] arr={1,20,30,5};
+//        int []x=arr; //x is shallow copy of arr
+//        x[0]=19;
+//        System.out.println(arr[0]);
+
+//        int[]arr={2,4,5,6,7};
+//        int[] y=Arrays.copyOf(arr,arr.length);//deep copy
+//        y[0]=90;
+//        System.out.println(arr[0]);
+
+    }
+}
