8776ff31ef18ff06ab28761ac5fa4bc2aab62da9
Meenakshi Singh <meenakshisingh0722@gmail.com>
2026-04-29 00:02:32 +0530

    Add files via upload

---
+++ whileloop.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+public class whileloop {
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+//        int i=1;
+//        while(i<=10){
+//            System.out.println(i);
+//            i++;
+//        }
+
+//        int i=1;
+//        do{
+//            System.out.println(i);
+//            i++;
+//        }while(i<=10);
+
+        //INFINITE LOOP
+//        for(int i=1;i<=10;i--){
+//            System.out.println(i);
+//        }
+//        while(1<2){
+//            System.out.println("ak");
+//        }
+
+        //(1) cound digit of a number 99=2
+//        int n =sc.nextInt();
+//        if(n==0) n=1;
+//        int count=0;
+//        while(n!=0){
+//            n/=10;
+//            count++;
+//        }System.out.println(count);
+
+        //(2) print sum of digits of a num 123=1+2+3
+//        int n=sc.nextInt();
+//        int sum=0;
+//        if(n<0) ;n=-n;
+//        while(n != 0) {
+//            sum+=n%10;
+//            n=n/10;
+//        }System.out.println((sum) ;
+
+        //(3) print max number of integer
+//        int x=Integer.MAX_VALUE;
+//        long y=(long)2147483647+10;
+//        System.out.println(x);
+
+        //(4)reverse a number
+//        int n=sc.nextInt();
+//        int r=0;
+//        while(n!=0){
+//            r*=10;
+//            r+=(n%10);
+//            n/=10;
+//        }System.out.print(r);
+
+        //(5)print sum of number and its reverse
+//        int n=sc.nextInt();
+//        int sum=0, r=0;
+//        if(n<0) n=-n;
+//        while(n!=0){
+//            sum+=n%10;
+//            r*=10;
+//            r+=(n%10);
+//            n=n/10;
+//        }System.out.println("SUM IS "+sum+" "+"& REVERSE NUM IS "+r );
+
+        //(6) factorial of a number
+//        int n=sc.nextInt();
+//        int fact=1;
+//        for(int i=1;i<=n;i++){
+//            fact=fact*i;
+//        }System.out.println(fact);
+
+        //a raise to the power b
+//        int a=sc.nextInt();
+//        int b=sc.nextInt();
+//        int p=1;
+//        for(int i=1;i<=b;i++){
+//            p*=a;
+//        }System.out.println(p);
+
+    }
+}
